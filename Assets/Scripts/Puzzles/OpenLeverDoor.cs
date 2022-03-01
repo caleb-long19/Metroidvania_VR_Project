41c9b2a267af7cfdb8a583fc6f39cc925ce3d134
Caleb Long <caleblong199@gmail.com>
2022-03-01 02:33:53 +0000

    The Hallowed VR - Changelog 13 - More Puzzles
    Added: Destroy All Targets Puzzle (OpenTargetDoor Script)
     - Added Script that detects if all Targets have been destroyed, if they are all gone, the door will open
    Added: Functional Levers (OpenLeverDoor Script)
     - When the Lever reaches a certain rotational point, the assigned door will open.
    Added: DestroyTarget Script
     - If a game object with a tag of "Bolt" hits the Target game object, it will destroy it.
    
    Removed some useless scripts e.g. ActivateTarget & ActivateLever

--- Assets/Scripts/Puzzles/OpenLeverDoor.cs
+++ Assets/Scripts/Puzzles/OpenLeverDoor.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class OpenLeverDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject leverObject;
+    private float leverXposition;
+    public Animator OpenFirstLeverDoor;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.hasChanged)
+        {
+            leverXposition = leverObject.transform.localRotation.eulerAngles.x;
+            leverXposition = (leverXposition > 180) ? leverXposition - 360 : leverXposition;
+            Debug.Log("Current Lever Position: " + leverXposition);
+
+            if (leverXposition >= 35)
+            {
+
+                Debug.Log("LEVER IS WORKING!");
+                OpenFirstLeverDoor.SetBool("LeverActivated", true);
+            }
+        }
     }
 }
